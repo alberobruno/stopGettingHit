6636521659cda8486a5980b1679a86d67c7499d2
Bruno Albero <alberobruno@gmail.com>
2022-07-12 11:34:35 -0400

    successfully created base

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import App from './components/App';
+
+reactDom.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
