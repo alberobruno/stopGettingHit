86f3f9c005a6b09be3b3757de31e735847b5f6c6
Bruno Albero <alberobruno@gmail.com>
2023-12-08 19:36:11 -0500

    First file converted to ts

--- src/components/PlayerStats.tsx
+++ src/components/PlayerStats.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import {
   TableContainer,
   Table,
@@ -8,19 +8,14 @@ import {
   TableCell,
 } from '@carbon/react';
 
-// interface PlayerStatsProps {
-//   playerMoves: any;
-//   p1MainPlayer: boolean;
-// }
+interface PlayerStatsProps {
+  playerMoves: string[][];
+  p1MainPlayer: boolean;
+}
 
-export default function PlayerStats({ playerMoves, p1MainPlayer }) {
-  console.log('PlayerMoves: ', playerMoves);
-  console.log('p1MainPlayer: ', p1MainPlayer);
-
+export default function PlayerStats({ playerMoves, p1MainPlayer }: PlayerStatsProps) {
   const p1BeatenBy = playerMoves[0];
   const p2BeatenBy = playerMoves[1];
-
-  // Add in moves that lost
   const movesPerformed = 'To be added in future version';
 
   return (
@@ -36,14 +31,15 @@ export default function PlayerStats({ playerMoves, p1MainPlayer }) {
           </TableHead>
           <TableBody>
             {(p1MainPlayer ? p1BeatenBy : p2BeatenBy).map((move, i) => (
-              <TableRow key={i}>
+                <React.Fragment key={i}>
+              <TableRow>
                 <TableCell>{move}</TableCell>
                 <TableCell>{movesPerformed}</TableCell>
               </TableRow>
+              </React.Fragment>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
-    </>
-  );
+      </>  );
 }
